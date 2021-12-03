de3ce5fd378cc5d24a5fcc150c529009453bc16d
Mikadifo <michaelpadillaheredia@hotmail.com>
2021-12-02 21:54:16 -0500

    Feat: tutor empresaria

--- src/app/pages/anexos/anexos.component.ts
+++ src/app/pages/anexos/anexos.component.ts
@@ -14,4 +14,30 @@ export class AnexosComponent implements OnInit {
   getAnexo() {
     return this.route.url._value[1].path;
   }
+
+  getStudents() {
+    return [
+      {
+        id: "0104640974",
+        names: "Michael Anthony",
+        last: "Padilla Heredia",
+        email: "michael.padilla.est@tecazuay.edu.ec",
+        phone: "0996082041",
+      },
+      {
+        id: "0104640974",
+        names: "Jose Anthony",
+        last: "Perez Hernesto",
+        email: "michael.padilla.est@tecazuay.edu.ec",
+        phone: "0996082041",
+      },
+      {
+        id: "0104640974",
+        names: "Pedro Mauricio",
+        last: "Ordonez Salto",
+        email: "michael.padilla.est@tecazuay.edu.ec",
+        phone: "0996082041",
+      },
+    ];
+  }
 }
