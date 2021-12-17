9594e1568f40e450e444f16f57bb236c6eb52015
icrv93 <icrv93@gmail.com>
2021-12-17 10:21:49 -0500

    updateppp

--- src/app/pages/add-convocatoria/convocatoria.component.ts
+++ src/app/pages/add-convocatoria/convocatoria.component.ts
@@ -1,6 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Carrera } from 'src/app/models/carrera.model';
 import { Docente } from 'src/app/models/docente.model';
@@ -8,22 +8,27 @@ import { Empresa } from 'src/app/models/empresa.model';
 import { CarreraService } from 'src/app/services/services.models/carrera.service';
 import { DocenteService } from 'src/app/services/services.models/docente.service';
 import { EmpresaService } from 'src/app/services/services.models/empresa.service';
-import Swal from 'sweetalert2';
+
 import * as moment from 'moment';
 import { ThrowStmt } from '@angular/compiler';
 import { formatDate } from '@angular/common';
 import { Convocatoria } from 'src/app/models/convocatoria.model';
 import { ConvocatoriasService } from 'src/app/services/services.models/convocatorias.service';
 import { Variable } from '@angular/compiler/src/render3/r3_ast';
-
+import { environment } from 'src/environments/environment';
+const bd_url = environment.bd_url;
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-convocatoria',
   templateUrl: './convocatoria.component.html',
   styleUrls: ['./convocatoria.component.css']
 })
 export class ConvocatoriaComponent implements OnInit {
-  public formSubmitted = false;
-  public carrera = new Carrera();
+  autocompleteControl = new FormControl();
+  public convocatoria = new Convocatoria();
+   public formSubmitted = false;
+  public bd_url = bd_url + "/personas";
+
   //VARIABLE DE CARRERAS
   public carreras: Carrera[] = [];
   //VARIABLE DE DOCENTES
@@ -41,7 +46,7 @@ export class ConvocatoriaComponent implements OnInit {
   //VARIABLE DE CONVOCATORIAS
   public convocatorias: Convocatoria [] =[];
   ultimoElemento=1;
-  //Variable fecha 
+  //Variable fecha
   today = new Date();
   jstoday = '';
 
@@ -49,20 +54,21 @@ export class ConvocatoriaComponent implements OnInit {
     private docenteService: DocenteService,
     private carreraService: CarreraService,
     private empresaService: EmpresaService,
-    private convocatoriaService: ConvocatoriasService
+    private convocatoriaService: ConvocatoriasService,
+    private miDatePipe: DatePipe,
+  private router: Router,
 
 
   ) {
     this.jstoday = formatDate(this.today, 'dd-MM-yyyy hh:mm:ss a', 'en-US', '+0530');
-    
+
   }
 
   ngOnInit() {
     this.getCarreras();
     this.getEmpresas();
     this.getDocentes();
-    this.getConvocatorias();
-   
+    this.numeroconvocatoria();
   }
 
   private getCarreras() {
@@ -84,16 +90,70 @@ export class ConvocatoriaComponent implements OnInit {
     });
   }
 
-  private getConvocatorias() {
+  private numeroconvocatoria() {
     this.convocatoriaService.getConvocatorias().subscribe((convocatorias) => {
-      
+
       this.convocatorias = convocatorias;
       this.ultimoElemento = (convocatorias.length - 1)+1;
-      
+
     });
   }
 
-  private guardarConvocatoria(form: NgForm) {
+
+  guardarConvocatoria(form: NgForm) {
+    this.formSubmitted = true;
+    if (form.invalid) {
+      return;
+    }
+    if (this.convocatoria.id) {
+      const fechaFormateadaInicio = this.miDatePipe.transform(
+        this.convocatoria.fecha_emision,
+        "yyyy-MM-dd"
+      );
+      this.convocatoria.fecha_emision = fechaFormateadaInicio;
+
+      const fechaFormateadaFin = this.miDatePipe.transform(
+        this.convocatoria.fecha_max_recib_solic,
+        "yyyy-MM-dd"
+      );
+      this.convocatoria.fecha_max_recib_solic = fechaFormateadaFin;
+
+      this.convocatoriaService
+        .editar(this.convocatoria, this.convocatoria.id)
+        .subscribe((convocatoria) => {
+          Swal.fire(
+            "Actualizar Carrera",
+            `¡${convocatoria.id} actualizada con exito!`,
+            "success"
+          );
+          this.irListaCarreras();
+        });
+    } else {
+      const fechaFormateadaInicio = this.miDatePipe.transform(
+        this.convocatoria.fecha_emision,
+        "yyyy-MM-dd"
+      );
+      this.convocatoria.fecha_emision = fechaFormateadaInicio;
+
+      const fechaFormateadaFin = this.miDatePipe.transform(
+        this.convocatoria.fecha_max_recib_solic,
+        "yyyy-MM-dd"
+      );
+      this.convocatoria.fecha_max_recib_solic = fechaFormateadaFin;
+      this.convocatoriaService.crear(this.convocatoria).subscribe((convocatoria) => {
+        Swal.fire(
+          "Nueva Carrera",
+          `¡${convocatoria.id} creada con exito!`,
+          "success"
+        );
+        this.irListaCarreras();
+      });
+    }
   }
-  
+
+  irListaCarreras() {
+    this.router.navigateByUrl("/dashboard/carreras");
+  }
+
+
 }
--- src/app/services/services.models/convocatorias.service.ts
+++ src/app/services/services.models/convocatorias.service.ts
@@ -1,20 +1,59 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from "rxjs/operators";
 import { Convocatoria } from 'src/app/models/convocatoria.model';
 import { ConvocatoriaComponent } from 'src/app/pages/add-convocatoria/convocatoria.component';
 import { environment } from 'src/environments/environment';
+import Swal from "sweetalert2";
 const bd_url = environment.bd_url + "/carreras";
 @Injectable({
 
   providedIn: 'root'
 })
 export class ConvocatoriasService {
- 
+
   constructor(private http: HttpClient) { }
 
-   //CONVOCATORIAS 
+   //CONVOCATORIAS
    getConvocatorias(): Observable<Convocatoria[]> {
     return this.http.get<Convocatoria[]>(`${bd_url}/filtrar`);
+  }
+
+
+  //CREAR Carrera SIN FOTO
+  crear(convocatoria: Convocatoria): Observable<Convocatoria> {
+    return this.http.post<Convocatoria>(`${bd_url}/`, convocatoria).pipe(
+      map((response: any) => response.carrera as Convocatoria),
+      catchError((e) => {
+        if (e.status == 400) {
+          return throwError(e);
+        }
+        Swal.fire(e.error.mensaje, e.error.error, "error");
+        return throwError(e);
+      })
+    );
+  }
+  //EDITAR CONVOCATORIA
+  editar(convocatoria: Convocatoria, id: number): Observable<Convocatoria> {
+    return this.http.put<Convocatoria>(`${bd_url}/${id}`, convocatoria).pipe(
+      map((response: any) => response.convocatoria as Convocatoria),
+      catchError((e) => {
+        if (e.status == 400) {
+          return throwError(e);
+        }
+        Swal.fire(e.error.mensaje, e.error.error, "error");
+        return throwError(e);
+      })
+    );
+  }
+  //ELIMINAR UNA CONVOCATORIA
+  eliminar(id: number): Observable<Convocatoria> {
+    return this.http.delete<Convocatoria>(`${bd_url}/${id}`).pipe(
+      catchError((e) => {
+        Swal.fire(e.error.mensaje, e.error.error, "error");
+        return throwError(e);
+      })
+    );
   }
 }
