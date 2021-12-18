8a94ca325a7802f1ac64edae41b9da705ed512e7
Pucho <95262345+pucho1077@users.noreply.github.com>
2021-12-18 16:53:08 -0500

    servicio personalempresa y vista DE DOCENTE

--- src/app/services/services.models/empresa-personal.service.ts
+++ src/app/services/services.models/empresa-personal.service.ts
@@ -1,13 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { EmpresaPersonal } from 'src/app/models/empresaPersonal.model';
 
 import { environment } from "src/environments/environment";
 
 import Swal from "sweetalert2";
 
-const bd_url = environment.bd_url + "/carreras";
+const bd_url = environment.bd_url + "/empresas_personales";
 
 @Injectable({
   providedIn: 'root'
@@ -15,13 +16,75 @@ const bd_url = environment.bd_url + "/carreras";
 export class EmpresaPersonalService {
   constructor(private http: HttpClient) {}
 
-  //OBTENER UNA EmpresaPersonal POR ID
-  getDocenteById(id: number): Observable<EmpresaPersonal> {
-    return this.http.get<EmpresaPersonal>(`${bd_url}/${id}`);
-  }
+ //OBTENER UNA EmpresaPersonal POR ID
+ getById(id: number): Observable<EmpresaPersonal> {
+  return this.http.get<EmpresaPersonal>(`${bd_url}/${id}`);
+}
 
-  //CARRERAS SIN PAGINACION
-  getEmpresaPersonal(): Observable<EmpresaPersonal[]> {
-    return this.http.get<EmpresaPersonal[]>(`${bd_url}/filtrar`);
-  }
+//CARRERAS SIN PAGINACION
+getEmpresaPersonal(): Observable<EmpresaPersonal[]> {
+  return this.http.get<EmpresaPersonal[]>(`${bd_url}/filtrar`);
+}
+
+//PAGINACION DE CARRERAS
+getEmpresaPersonalPage(
+  page: string,
+  size: string,
+  busqueda: string
+): Observable<any> {
+  return this.http
+    .get(
+      `${bd_url}/page?page=${page}&size=${size}&busqueda=${busqueda || ""} `
+    )
+    .pipe(
+      tap((response: any) => {
+        (response.content as EmpresaPersonal[]).forEach((EmpresaPersonal) => {
+          return EmpresaPersonal;
+        });
+      }),
+      map((response: any) => {
+        (response.content as EmpresaPersonal[]).map((EmpresaPersonal) => {
+          return EmpresaPersonal;
+        });
+        return response;
+      })
+    );
+}
+
+//CREAR EmpresaPersonal
+crear(empresaPersonal: EmpresaPersonal): Observable<EmpresaPersonal> {
+  return this.http.post<EmpresaPersonal>(`${bd_url}/`, empresaPersonal).pipe(
+    map((response: any) => response.empresaPersonal as EmpresaPersonal),
+    catchError((e) => {
+      if (e.status == 400) {
+        return throwError(e);
+      }
+      Swal.fire(e.error.mensaje, e.error.error, "error");
+      return throwError(e);
+    })
+  );
+}
+
+//EDITAR EmpresaPersonal
+editar(empresaPersonal: EmpresaPersonal, id: number): Observable<EmpresaPersonal> {
+  return this.http.put<EmpresaPersonal>(`${bd_url}/${id}`, empresaPersonal).pipe(
+    map((response: any) => response.empresaPersonal as EmpresaPersonal),
+    catchError((e) => {
+      if (e.status == 400) {
+        return throwError(e);
+      }
+      Swal.fire(e.error.mensaje, e.error.error, "error");
+      return throwError(e);
+    })
+  );
+}
+//ELIMINAR UN EmpresaPersonal
+eliminar(id: number): Observable<EmpresaPersonal> {
+  return this.http.delete<EmpresaPersonal>(`${bd_url}/${id}`).pipe(
+    catchError((e) => {
+      Swal.fire(e.error.mensaje, e.error.error, "error");
+      return throwError(e);
+    })
+  );
+}
 }
