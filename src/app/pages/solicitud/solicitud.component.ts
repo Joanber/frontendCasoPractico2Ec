0a9d430c271e73580a26ca35319e950a5cb7b6d5
GraceMentvil <graelvilmen@gmail.com>
2021-12-20 20:10:08 -0500

    Solicitud Estudiante
    Anexo 3 Solicitud Estudiante

--- src/app/pages/solicitud/solicitud.component.ts
+++ src/app/pages/solicitud/solicitud.component.ts
@@ -1,9 +1,11 @@
 import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Carrera } from 'src/app/models/carrera.model';
+import { Convocatoria } from 'src/app/models/convocatoria.model';
 import { Docente } from 'src/app/models/docente.model';
 import { Empresa } from 'src/app/models/empresa.model';
 import { CarreraService } from 'src/app/services/services.models/carrera.service';
+import { ConvocatoriasService } from 'src/app/services/services.models/convocatorias.service';
 import { DocenteService } from 'src/app/services/services.models/docente.service';
 import { EmpresaService } from 'src/app/services/services.models/empresa.service';
 
@@ -13,17 +15,21 @@ import { EmpresaService } from 'src/app/services/services.models/empresa.service
   styleUrls: ['./solicitud.component.css']
 })
 export class SolicitudComponent implements OnInit {
- //VARIABLE DE CARRERAS
+//VARIABLE DE CARRERAS
  public carreras: Carrera[] = [];
- //VARIABLE DE DOCENTES
+//VARIABLE DE DOCENTES
  public docentes: Docente[] = [];
+//VARIABLE DE EMPRESA
  public empresas: Empresa[] = [];
-//Variable fecha 
+ //VARIABLE DE EMPRESA
+ public convocatorias: Convocatoria[] = [];
+//VARIABLE DE FECHA 
 today = new Date();
 jstoday = '';
 
   constructor(
     private empresasService: EmpresaService,
+    private convocatoriasService: ConvocatoriasService,
     private docenteService: DocenteService,
     private carreraService: CarreraService) {
     this.jstoday = formatDate(this.today, 'dd-MM-yyyy hh:mm:ss a', 'en-US', '+0530');
@@ -34,6 +40,8 @@ jstoday = '';
     this.getCarreras();
     this.getDocentes();
     this.getEmpresas();
+    this.getConvocatorias();
+    
   }
   private getDocentes() {
     this.docenteService.getDocentes().subscribe((docentes) => {
@@ -50,6 +58,11 @@ jstoday = '';
   private getEmpresas() {
     this.empresasService.getEmpresas().subscribe((empresas) => {
       this.empresas = empresas;
+    });
+  }
+  private getConvocatorias() {
+    this.convocatoriasService.getConvocatorias().subscribe((convocatorias) => {
+      this.convocatorias = convocatorias;
     });
   }
 
