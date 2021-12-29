e5ef30c101f4d63c39bd90b94c53dd58ee4577e7
Christian Caguana <77030314+ChristianCaguana@users.noreply.github.com>
2021-12-29 16:30:37 -0500

    ContinuandoPDF

--- src/app/pages/add-convocatoria/convocatoria.component.ts
+++ src/app/pages/add-convocatoria/convocatoria.component.ts
@@ -10,6 +10,8 @@ import Swal from 'sweetalert2';
 import { SolicitudEmpresa } from 'src/app/models/solicitudEmpresa.model';
 import { SolicitudEmpresaService } from 'src/app/services/services.models/solicitud-empresa.service';
 import jsPDF, * as jspdf from 'jspdf';
+import { style } from '@angular/animations';
+import { ThrowStmt } from '@angular/compiler';
 
 
 interface jsPDFWithPlugin extends jspdf.jsPDF {
@@ -165,13 +167,21 @@ export class ConvocatoriaComponent implements OnInit {
     // const head = [['No', 'Fecha','Hora llegada', 'Hora salida', 'Actividad realizada', 'Firma tutor', 'Numero horas']];
     const doc = new jsPDF('p', 'pt', 'a4');
     doc.setFontSize(14);
-    doc.text('ANEXO 2: Convocatoria a prácticas pre profesionales ', 20, 20);
-    doc.text('INSTITUTO SUPERIOR TECNOLÓGICO DEL AZUAY', 50, 50);
-    // doc.text(data[0].solicitudEmpresa.responsablePPP.carrera.nombre, 30,70);
-    doc.text('A los estudiantes Interesados:', 10, 90);
+    doc.text('ANEXO 2: Convocatoria a prácticas pre profesionales ', 20, 40);
+    doc.text('INSTITUTO SUPERIOR TECNOLÓGICO DEL AZUAY', 20, 100);
+    doc.text('A los estudiantes Interesados: ', 20, 120);
+    doc.text('Se convoca a los estudiantes de '+this.convocatoria.ciclo, 20, 140);
+    doc.text('ciclo de la carrera '+this.solicitudEmpresa.responsablePPP.carrera.nombre,20, 160);
+    doc.text('que deseen realizar sus prácticas pre profesionales en la empresa '+this.solicitudEmpresa.empresa.nombre,20, 180);
+    doc.text('a presentar la solicitud correspondiente.',20, 200);
+    doc.text('Las actividades a desarrollar son: '+this.convocatoria.actividades,20,220);
+    doc.text('por lo que los postulantes deberán haber aprobado las siguientes asignaturas:'+this.convocatoria.asignaturas,20,260);
+    doc.text('La fecha máxima en la que se receptarán las solicitudes es el '+this.convocatoria.fecha_max_recib_solic,20,280);
+    doc.text('Para mayor información contactarse con '+this.solicitudEmpresa.responsablePPP.docente.persona.primer_nombre+this.solicitudEmpresa.responsablePPP.docente.persona.primer_apellido,20,300);
+    
     doc.setFontSize(10);
-    // doc.text("Estudiante: "+data[0].estudiante.persona.primernombre + ' ' +data[0].estudiante.persona.primerapellido,
-    //   10,110);
+    //  doc.text("Estudiante: "+data[0].estudiante.persona.primernombre + ' ' +data[0].estudiante.persona.primerapellido,
+    //    10,110);
     // doc.text("Empresa: "+ data[0].tutorEmpresarial.empresa.nombreempresa ,10,130);
     // doc.text("Tutor empresarial: "+ data[0].tutorEmpresarial.nombretutor ,10,150);
     // doc.text("Horas cumplidas: " +this.totalH.toString()+" horas" , 10,170);
