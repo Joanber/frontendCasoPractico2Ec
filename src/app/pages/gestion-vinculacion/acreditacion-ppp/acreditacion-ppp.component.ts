0e641dbfe0a85f1f97398d87bde7b41cf41a2aa3
RoyMorocho <morochoroy@gmail.com>
2021-12-30 20:24:59 -0500

    acreditacion ppp

--- src/app/pages/gestion-vinculacion/acreditacion-ppp/acreditacion-ppp.component.ts
+++ src/app/pages/gestion-vinculacion/acreditacion-ppp/acreditacion-ppp.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MatButton, MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { Persona } from 'src/app/models/persona.model';
 import Swal from 'sweetalert2';
 
 export interface Componente {
@@ -14,6 +15,8 @@ export interface Componente {
 export class AcreditacionPppComponent implements OnInit {
   // @ViewChild('cert', null) cert: ElementRef;
   @ViewChild(MatButton, null) button: MatButton;
+
+  cordinador: Persona;
 
   constructor(public dialog: MatDialog) {}
   today: string;
@@ -33,6 +36,8 @@ export class AcreditacionPppComponent implements OnInit {
   ];
 
   ngOnInit() {
+    this.cordinador = JSON.parse(localStorage.getItem('usuario')).persona;
+
     const options = { year: 'numeric', month: 'long', day: 'numeric' } as const;
     this.today = this.date.toLocaleDateString(undefined, options);
   }
@@ -41,23 +46,30 @@ export class AcreditacionPppComponent implements OnInit {
     const estudiante = 'ÁLVAREZ TOLEDO JÉSSICA GUADALUPE';
     const cedula = '0107340093';
     const carrera = 'Tecnología Superior en Desarrollo de Software';
-    const body = `Una vez revisada la documentación entregada por el estudiante <b>${ estudiante }</b>,
-    portadora de la cédula de ciudadanía número <b>${ cedula }</b>, de la carrera de <b>${ carrera }</b>
+    const body = `Una vez revisada la documentación entregada por el estudiante <strong>${ estudiante }</strong>,
+    portadora de la cédula de ciudadanía número <strong>${ cedula }</strong>, de la carrera de <strong>${ carrera }</strong>
     del Instituto Superior Tecnológico del Azuay ha cumplido con todos los requisitos establecidos
-            en la ley para acreditar <b>SATISFACTORIAMENTE</b> las <b>400 horas</b> de prácticas pre profesionales,
+            en la ley para acreditar <strong>SATISFACTORIAMENTE</strong> las <strong>400 horas</strong> de prácticas pre profesionales,
             las cuales fueron desarrolladas cumpliendo con los dos componentes dispuestos en el Artículo 53
             del Reglamento de Régimen Académico y en el proyecto de la carrera correspondiente:`;
     // this.cert.nativeElement.innerHTML = body;
     return body;
   }
 
+  get NombreCoordinador(): string {
+    const nombre = this.cordinador.primer_nombre + ' ' + this.cordinador.primer_apellido;
+    return nombre.charAt(0).toUpperCase() + nombre.slice(1).toLocaleLowerCase();
+
+  }
 
   openDialog(): void {
     const dialogRef = this.dialog.open(DialogAcreditacionComponent, {
       data: {
-        date: this.date, today: this.today, componentes: this.componentes, bodyCertificado: this.bodyCertificado
+        date: this.date, today: this.today, componentes: this.componentes,
+        bodyCertificado: this.bodyCertificado, coordinador: this.NombreCoordinador
       },
     });
+
 
     dialogRef.afterClosed().subscribe(result => {
       if (result === true) {
@@ -87,7 +99,7 @@ export interface DialogData {
   today: string;
   componentes: Componente;
   bodyCertificado: string;
-
+  coordinador: string;
 }
 
 @Component({
@@ -98,11 +110,10 @@ export interface DialogData {
 export class DialogAcreditacionComponent implements AfterViewInit {
   @ViewChild('bodyCertificado', null) bodyCertificado: ElementRef;
   constructor(public dialogRef: MatDialogRef<DialogAcreditacionComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
   }
-
   ngAfterViewInit(): void {
-      this.bodyCertificado.nativeElement.innerHTML = this.data.bodyCertificado;
+    this.bodyCertificado.nativeElement.innerHTML = this.data.bodyCertificado;
   }
 
   onNoClick(): void {
