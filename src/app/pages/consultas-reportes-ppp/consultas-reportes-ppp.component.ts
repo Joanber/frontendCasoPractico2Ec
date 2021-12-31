a46189e2c1ca834ff482a2cdd4e5a300d22b4a6c
icrv93 <icrv93@gmail.com>
2021-12-31 16:10:17 -0500

    reporteTutores Termiando

--- src/app/pages/consultas-reportes-ppp/consultas-reportes-ppp.component.ts
+++ src/app/pages/consultas-reportes-ppp/consultas-reportes-ppp.component.ts
@@ -1,32 +1,14 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import {
-  MatPaginator,
-  MatPaginatorIntl,
-  MatSort,
-  MatTableDataSource,
-  PageEvent,
-  Sort,
-} from "@angular/material";
+import { MatPaginator, PageEvent } from "@angular/material";
 import autoTable, { UserOptions } from "jspdf-autotable";
 import { Carrera } from "src/app/models/carrera.model";
-import { Convocatoria } from "src/app/models/convocatoria.model";
-import { Empresa } from "src/app/models/empresa.model";
-import { Persona } from "src/app/models/persona.model";
 import { CarreraService } from "src/app/services/services.models/carrera.service";
-import { ConvocatoriasService } from "src/app/services/services.models/convocatorias.service";
-import { EmpresaService } from "src/app/services/services.models/empresa.service";
-import { PersonaService } from "src/app/services/services.models/persona.service";
 import { environment } from "src/environments/environment";
-import Swal from "sweetalert2";
 import jsPDF, * as jspdf from "jspdf";
 import { DesignacionTA } from "src/app/models/designacionta.model";
-import { FormControl } from "@angular/forms";
-import { LoaderService } from "src/app/services/interceptores/loader.service";
-import { LiveAnnouncer } from "@angular/cdk/a11y";
 import { DesignacionTaService } from "src/app/services/services.models/designacion-ta.service";
 import { ValidacionSAC } from "src/app/models/validaciones_sac.model";
 import { ValidacionesSacService } from "src/app/services/services.models/validaciones-sac.service";
-import { Router } from "@angular/router";
 import { DesignacionTE } from "src/app/models/designacionte.model";
 import { DesignacionTEService } from "src/app/services/services.models/designacion-te.service";
 
@@ -58,15 +40,18 @@ export class ConsultasReportesPppComponent implements OnInit {
   public designacionta = new DesignacionTA();
   public designacionte = new DesignacionTE();
 
+  seleccionado:string;
+
+  listTutores = [
+      "Tutores Academico" ,
+    "Tutores empresariales" ,
+  ];
   constructor(
     private validacionesSacService: ValidacionesSacService,
     private carreraService: CarreraService,
     private designacionTAService: DesignacionTaService,
-   private designacionTEService: DesignacionTEService
-  ) {
-
-
-  }
+    private designacionTEService: DesignacionTEService
+  ) {}
 
   ngOnInit() {
     this.getDesignacinTA();
@@ -106,6 +91,7 @@ export class ConsultasReportesPppComponent implements OnInit {
         this.paginador._intl.lastPageLabel = "Última Página";
       });
   }
+
   cargarCarreras() {
     this.carreraService
       .getCarreras()
@@ -128,7 +114,6 @@ export class ConsultasReportesPppComponent implements OnInit {
         this.totalPorPagina.toString(),
         this.carreraFiltro
       );
-
     } else {
       return;
     }
